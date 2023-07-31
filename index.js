6a0c4b613f89720216f848ba0d22ee9fdecedb3d
JakobJohnson <madjzj@gmail.com>
2023-07-31 14:49:03 -0700

    Completed Lab

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// Write your code here!
+// Write your code her
+const element = document.getElementById("main");
+element.remove();
+const newHeader = document.createElement("h1")
+newHeader.id="victory"
+newHeader.textContent="Jakob is the champion"
